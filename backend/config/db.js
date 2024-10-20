c0e03fd8cd7c6ec085bb8e7d3c7e9f065a91deff
Harshit <harshit.sansanwal2528@gmail.com>
2024-10-20 22:15:23 +0530

    #429 Rename branch "SuSE-RES-9-Branch" to "SuSE-SLL-9-Branch"

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect("mongodb://localhost:27017/")
     }catch(err){
         console.log(err)
     }
